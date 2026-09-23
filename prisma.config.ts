22403e6b117dee5280c2e817c3ce37796cdc6971
psy0821-k <p49417875@gmail.com>
2026-09-23 14:12:06 +0900

    fix: prisma 8.X => 7.X 로 다운그레이드 사유 :  아직 안정적인 버전이 검증안됨

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,12 @@
-import { definePrismaConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
-export default definePrismaConfig({
-  skills: {
-    agents: ["claude", "cursor", "agents", "devin"],
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
   },
 });
